e8a7782312124f3bc09cf2fab33c2a3a1a1b7914
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-04-11 14:49:12 +0300

    Created the 'login' controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,26 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
+const config = require('../configuration/config');
+const words = require('../configuration/words');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+
+const signToken = (_id) => jwt.sign(
+  { _id },
+  config.JWT_SECRET,
+  { expiresIn: config.JWT_EXPIRES_IN },
+);
+
+const createSendToken = (res, statusCode, user) => {
+  const token = signToken(user._id);
+
+  res.status(statusCode).send({
+    status: 'success',
+    token,
+    data: { user },
+  });
+};
 
 exports.createUser = catchAsync(async (req, res) => {
   const user = await User.create(req.body);
@@ -10,4 +31,23 @@ exports.createUser = catchAsync(async (req, res) => {
     status: 'success',
     data: { user },
   });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email })
+    .select('+password')
+    .orFail(new UnauthorizedError(words.INVALID_EMAIL_OR_PASSWORD));
+
+  const correctPassword = await user.isPasswordCorrect(password, user.password);
+
+  if (!correctPassword) {
+    next(new UnauthorizedError(words.INVALID_EMAIL_OR_PASSWORD));
+    return;
+  }
+
+  user.password = undefined;
+
+  createSendToken(res, 200, user);
 });
