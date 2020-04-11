ca0a1de8117801c7d6c7120c066156b68475c3b1
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-04-11 17:11:04 +0300

    Created the 'authPreValidator'
    Created the following middlewares:
    1) createUserReqCheck;
    2) loginReqCheck.

---
+++ middlewares/authPreValidator.js
@@ -0,0 +1,56 @@
+const { celebrate, Joi, Segments } = require('celebrate');
+
+const words = require('../configuration/words');
+
+const messages = {
+  name: {
+    'string.base': words.INVALID_REQUEST,
+    'any.required': words.USER_NAME_REQUIRED,
+    'string.empty': words.USER_NAME_REQUIRED,
+    'string.min': words.USER_NAME_MIN_LENGTH,
+    'string.max': words.USER_NAME_MAX_LENGTH,
+  },
+  email: {
+    'string.base': words.INVALID_REQUEST,
+    'any.required': words.USER_EMAIL_REQUIRED,
+    'string.empty': words.USER_EMAIL_REQUIRED,
+    'string.email': words.USER_EMAIL_IS_EMAIL,
+  },
+  password: {
+    'string.base': words.INVALID_REQUEST,
+    'any.required': words.USER_PASSWORD_REQUIRED,
+    'string.empty': words.USER_PASSWORD_REQUIRED,
+    'string.min': words.USER_PASSWORD_MIN_LENGTH,
+  },
+};
+
+exports.createUserReqCheck = celebrate({
+  [Segments.BODY]: Joi.object({
+    name: Joi.string()
+      .required()
+      .min(2)
+      .max(30)
+      .messages(messages.name),
+    email: Joi.string()
+      .required()
+      .email()
+      .messages(messages.email),
+    password: Joi.string()
+      .required()
+      .min(8)
+      .messages(messages.password),
+  }).prefs({ stripUnknown: true }),
+});
+
+exports.loginReqCheck = celebrate({
+  [Segments.BODY]: Joi.object({
+    email: Joi.string()
+      .required()
+      .email()
+      .messages(messages.email),
+    password: Joi.string()
+      .required()
+      .min(8)
+      .messages(messages.password),
+  }).prefs({ stripUnknown: true }),
+});
