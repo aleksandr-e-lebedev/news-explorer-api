2ccb0a7030c5f09b98ec1d168b44d4d02c45669f
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-04-12 18:29:56 +0300

    Mounted the 'errorController' to the app level

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const routes = require('./routes');
 const logger = require('./middlewares/appLogger');
+const errorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(logger.requestLogger);
 app.use(routes);
 
 app.use(logger.errorLogger);
+app.use(errorHandler);
 
 module.exports = app;
